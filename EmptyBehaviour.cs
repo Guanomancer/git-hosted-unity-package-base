e253913180c1a968a4e4ca38b8259f10901312d4
Zefugi <trowi@live.dk>
2021-03-22 18:30:05 +0100

    Created the basic package implementation.

---
+++ EmptyBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PackageBase
+{
+    public class EmptyBehaviour : MonoBehaviour
+    {
+        // The purpose of this behaviour is, to make sure the Package Base package assembly will compile.
+    }
+}
